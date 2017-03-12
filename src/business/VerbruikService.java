451eb9cbd71ab07ac7f31a257867bca12fe5e341
0rbster <jeroen@dekruijf.com>
2017-03-12 19:49:57 +0100

    Initial commit

---
+++ src/business/VerbruikService.java
@@ -0,0 +1,30 @@
+package business;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+/**
+ * Created by Jeroen on 12-Mar-17.
+ */
+@WebService()
+public class VerbruikService {
+  @WebMethod
+  public String berekenVerbruik(String verkeerType, String kilometers) {
+    int km = Integer.parseInt(kilometers);
+    switch (verkeerType){
+      case "spits":
+        return "Verbruik: "+(0.900 * km);
+      case "normaal":
+        return "Verbruik: "+(0.400 * km);
+      case "snelrijden":
+        return "Verbruik: "+(1.2 * km);
+    }
+
+    return "0";
+  }
+  public static void main(String[] argv) {
+    Object implementor = new VerbruikService();
+    String address = "http://localhost:9000/VerbruikService";
+    Endpoint.publish(address, implementor);
+  }
+}
